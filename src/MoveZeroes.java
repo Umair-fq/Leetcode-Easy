82c958f9b7dc79e91e07621054863bcf3acfb039
Umair-fq <farooqumair.islu@gmail.com>
2023-08-04 11:03:06 +0500

    283. Move Zeroes
    In-place algorithm to move all zeros to the end of an integer array, preserving the order of non-zero elements.

--- src/MoveZeroes.java
+++ src/MoveZeroes.java
@@ -2,34 +2,27 @@
 //        while maintaining the relative order of the non-zero elements.
 //        Note that you must do this in-place without making a copy of the array.
 public class MoveZeroes {
+
     public void moveZeroes(int[] nums) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            //taking second pointer so that we will keep track of the next position
-                int j = i + 1;
-                //if both first element is zero and second element is not zero then swap them
-                if (nums[i] == 0 && nums[j] != 0) {
-                    int temp = nums[j];
-                    nums[j] = nums[i];
-                    nums[i] = temp;
-                } else if(nums[i] == 0 && nums[j] == 0) {                  // if both are zero, then increment j until we get nonzero element or length is reached
-                    while (j < nums.length && nums[j] == 0) {
-                        j++;
-                    }
-                    // if j is less than nums.length
-                    if (j < nums.length) {
-                        int temp = nums[j];
-                        nums[j] = nums[i];
-                        nums[i] = temp;
-                    }
+        int insertedPos = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[insertedPos++] = nums[i];
             }
         }
+        while (insertedPos < nums.length) {
+            nums[insertedPos++] = 0;
+        }
+
         for (int num : nums) {
             System.out.println(num);
         }
+        System.out.println(nums[insertedPos-1]);
     }
 
     public static void main(String[] args) {
         MoveZeroes moveZeroes = new MoveZeroes();
         moveZeroes.moveZeroes(new int[]{2,1});
+        moveZeroes.moveZeroes(new int[]{0,1,0,3,12});
     }
 }
